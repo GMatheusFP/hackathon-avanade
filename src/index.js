1b9fe4e50f57be52d17d17b6ec6a48a0c3c99fcf
GMatheusFP <gianpinheiro1@hotmail.com>
2020-11-03 20:30:36 -0300

    [Initial] Creation first configs

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const Routes = require('./routes');
+const app = express();
+
+app.use(express.json());
+app.use(Routes);
+app.listen(3000, () => {
+  console.log('Rodando na porta 3000');
+});
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const routes = Router();
+
+routes.get('/', () => {
+  console.log('teste de rota');
+});
+
+//Rotascliente
+
+// Rotas motorista
+module.exports = routes;
